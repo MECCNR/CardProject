200505a6b906864fb937eba3b653c420e515be46
MECCNR <e.migalckin@yandex.ru>
2022-09-28 15:08:17 +0900

    "v2"

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,5 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
